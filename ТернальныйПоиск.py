83e1d482b173b911dc39cecbe98dfc9bb9695195
baaad_ya <122220908+imsobab@users.noreply.github.com>
2025-11-14 13:07:36 +0300

    ТернальныйПоиск.py

--- ТернальныйПоиск.py
+++ ТернальныйПоиск.py
@@ -1,12 +1,10 @@
 def ternary_search(arr, target):
     left, right = 0, len(arr)-1
     while left <= right:
-        mid1 = left + (right - left) // 3
-        mid2 = right - (right - left) // 3
+        mid1 = left + (right - left) // 3    # Первая точка деления
+        mid2 = right - (right - left) // 3   # Вторая точка деления
         if arr[mid1] == target: return mid1
         if arr[mid2] == target: return mid2
-        if target < arr[mid1]: right = mid1 - 1
-        elif target > arr[mid2]: left = mid2 + 1
-        else: left, right = mid1 + 1, mid2 - 1
-    return -1
+        if target < arr[mid1]: right = mid1 - 1       # Ищем в левой трети
+        elif target > arr[mid2]: left = mid2 + 1      # Ищем в правой трети
 print(ternary_search([1, 3, 5, 7, 9, 11, 13, 15], 9))
